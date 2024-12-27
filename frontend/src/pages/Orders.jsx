33375ab911a1e1735e23160a6bbfc972345cf601
dilmiishara2002 <dilmiishara2000@gmail.com>
2024-12-27 22:43:51 +0530

    navbar

---
+++ frontend/src/pages/Orders.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Orders = () => {
+  return (
+    <div>
+      orders page
+      
+    </div>
+  )
+}
+
+export default Orders
---
+++ frontend/src/pages/PlaceOrder.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PlaceOrder = () => {
+  return (
+    <div>
+      palce order page
+    </div>
+  )
+}
+
+export default PlaceOrder
