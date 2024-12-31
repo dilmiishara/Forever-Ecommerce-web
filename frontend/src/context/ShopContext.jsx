f5040abaf1a78a36dacedbac96161dadba6e6f15
dilmiishara2002 <dilmiishara2000@gmail.com>
2024-12-31 23:03:02 +0530

    Add to cart

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,5 +1,6 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { products } from "../assets/assets";
+import { toast } from "react-toastify";
 
 export const ShopContext = createContext();
 
@@ -8,10 +9,47 @@ const ShopContextProvider = (props) => {
     const delivery_fee = 10;
     const [search, setSearch] = useState('');
     const [showSearch, setShowSearch] = useState(false); // Corrected from 'flase' to 'false'
+    const [cartItems, setCartItems] = useState({});
+
+    const addToCart = async (itemId, size) => {
+        if (!size) {
+            toast.error('Select Product Size');
+            return;
+        }
+    
+        let cartData = structuredClone(cartItems);
+        const key = `${itemId}_${size}`; // Use a combination of itemId and size as the key
+    
+        if (cartData[key]) {
+            cartData[key] += 1;
+        } else {
+            cartData[key] = 1;
+        }
+    
+        setCartItems(cartData);
+    };
+    
+
+    const getCartCount = () => {
+        let totalCount = 0;
+        for (const key in cartItems) {
+            try {
+                if (cartItems[key] > 0) {
+                    totalCount += cartItems[key];
+                }
+            } catch (error) {
+                console.error(error); // It's a good practice to log the error
+            }
+        }
+        return totalCount;
+    };
+    
 
     const value = {
         products, currency, delivery_fee,
-        search, setSearch, showSearch, setShowSearch
+        search, setSearch, showSearch, setShowSearch,
+        cartItems,addToCart,
+        getCartCount
     };
 
     return (
